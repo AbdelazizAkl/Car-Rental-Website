01444bb124826252b5e55b4aed71af07136dfcfa
youssefwaheed123 <youssefwaheed@gmail.com>
2023-12-28 15:51:25 +0200

    updated registration form

--- car-rental-frontend/react-car-rental-website/src/pages/LoginPage.tsx
+++ car-rental-frontend/react-car-rental-website/src/pages/LoginPage.tsx
@@ -1,90 +1,73 @@
 import React, { useState } from "react";
 import Button from "../components/Button";
 import Alert from "../components/Alert";
-import TextFields from "../components/TextFields";
-import "./LoginPage.css";
+import "../css/LoginRegister.css";
 
 const Login = () => {
   const [alertVisible, setAlertVisibility] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [inputs, setinputs] = useState({
-  //   email: "",
-  //   password: "",
-  // });
-
   const [warnemail, setwarnemail] = useState(false);
   const [warnpass, setwarnpass] = useState(false);
   const [danger, setdanger] = useState(true);
-
   const [eye, seteye] = useState(true);
-  const [pass, setpass] = useState("password");
-  function validateForm() {
-    if (!email || !password) {
-      return false;
-    }
-    return true;
-  }
+
+  function loginHandler() {}
 
   return (
-    <>
-      <div className="homePage">
-        <div className="container">
-          <div className="card">
-            <div className="form">
-              <div className="left-side">
-                <img src="https://mphclub.com/wp-content/uploads/2021/07/mercedes-benz-lineup-exotic-car-rental-mph-club.jpg" />
+    <div className="homePage">
+      <h1>KWAIZO'S CARS RENTAL</h1>
+      <div className="container">
+        <div className="card">
+          <div className="form">
+            <div className="left-side">
+              <img src="https://mphclub.com/wp-content/uploads/2021/07/mercedes-benz-lineup-exotic-car-rental-mph-club.jpg" />
+            </div>
+
+            <div className="right-side">
+              <div className="register">
+                <p>
+                  Not a member? <a href="/register">Register Now</a>
+                </p>
               </div>
 
-              <div className="right-side">
-                <div className="register">
-                  <p>
-                    Not a member? <a href="/register">Register Now</a>
-                  </p>
-                </div>
-
-                <div className="hello">
-                  <h2>Hello Again!</h2>
-                  <h4>Welcome back you have been missed! </h4>
-                </div>
-
-                <form>
-                  <div className="input_text">
-                    <input
-                      className={` ${warnemail ? "warning" : ""}`}
-                      type="text"
-                      placeholder="Enter Email"
-                      name="email"
-                      value={email}
-                      onChange={(event) => setEmail(event.target.value)}
-                    />
-                    <p className={` ${danger ? "danger" : ""}`}>
-                      <i className="fa fa-warning"></i>Please enter a valid
-                      email address.
-                    </p>
-                  </div>
-                  <div className="input_text">
-                    <input
-                      className={` ${warnpass ? "warning" : ""}`}
-                      type="password"
-                      placeholder="Enter Password"
-                      name="password"
-                      value={password}
-                      onChange={(event) => setPassword(event.target.value)}
-                    />
-                  </div>
-                  <div>
-                    <Button color="primary">Sign in</Button>
-                  </div>
-                </form>
-
-                <hr />
+              <div className="hello">
+                <h2>Hello Again!</h2>
+                <h4>Welcome back you have been missed! </h4>
               </div>
+
+              <form>
+                <div className="input_text">
+                  <input
+                    type="text"
+                    placeholder="Enter Email"
+                    name="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </div>
+                <div className="input_text">
+                  <input
+                    type="password"
+                    placeholder="Enter Password"
+                    name="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </div>
+                <div className="button">
+                  <Button color="primary" onClick={loginHandler}>
+                    Sign in
+                  </Button>
+                </div>
+              </form>
+
+              <hr />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default Login;
--- car-rental-frontend/react-car-rental-website/src/pages/RegisterPage.tsx
+++ car-rental-frontend/react-car-rental-website/src/pages/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Button from "../components/Button";
 import Alert from "../components/Alert";
-import TextFields from "../components/TextFields";
+import "../css/LoginRegister.css";
 
 // Now you can use the 'history' object as needed in your component
 
@@ -15,89 +15,113 @@ const Register = () => {
   const [address, setAddress] = useState("");
   const [license, setLicense] = useState("");
   const [confirmPassword, setConfirmedPassword] = useState("");
-  const [passwordsMatch, setPasswordsMatch] = useState(true);
 
-  const handlePasswordValidation = () => {
-    if (password !== confirmPassword) {
-      setPasswordsMatch(false);
-      setAlertVisibility(true);
-    } else {
-      setPasswordsMatch(true);
-    }
-  };
+  function registrationHandler() {}
 
   return (
-    <div>
-      <TextFields
-        label="First Name"
-        type="string"
-        placeholder="First name"
-        value={fname}
-        onChange={(event) => setFirstName(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label="Last Name"
-        type="string"
-        placeholder="Last name"
-        value={lname}
-        onChange={(event) => setLastName(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label="Address"
-        type="string"
-        placeholder="Address"
-        value={address}
-        onChange={(event) => setAddress(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label="Phone Number"
-        type="string"
-        placeholder="Phone number"
-        value={phone}
-        onChange={(event) => setPhoneNumber(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label="License"
-        type="string"
-        placeholder="License"
-        value={license}
-        onChange={(event) => setLicense(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label={"Email"}
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)} // Corrected onChange handler
-      />
-      <TextFields
-        label="Password"
-        type="password"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      <TextFields
-        label="Confirm Password"
-        type="password"
-        placeholder="Renter your password"
-        value={confirmPassword}
-        onChange={(event) => {
-          setConfirmedPassword(event.target.value);
-        }}
-      />
+    <>
+      <div className="homePage">
+        <div className="container">
+          <div className="card">
+            <div className="form">
+              <div className="left-side">
+                <img src="https://mphclub.com/wp-content/uploads/2021/07/mercedes-benz-lineup-exotic-car-rental-mph-club.jpg" />
+              </div>
+              <div className="right-side">
+                <div className="register">
+                  <p>
+                    Already a member? <a href="/">Login Now</a>
+                  </p>
+                </div>
+                <form>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter First Name"
+                      name="fname"
+                      value={fname}
+                      onChange={(event) => setFirstName(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter Last Name"
+                      name="lname"
+                      value={lname}
+                      onChange={(event) => setLastName(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter Phone Number"
+                      name="phone"
+                      value={phone}
+                      onChange={(event) => setPhoneNumber(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter Address"
+                      name="address"
+                      value={address}
+                      onChange={(event) => setAddress(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter License"
+                      name="license"
+                      value={license}
+                      onChange={(event) => setLicense(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="text"
+                      placeholder="Enter Email"
+                      name="email"
+                      value={email}
+                      onChange={(event) => setEmail(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="password"
+                      placeholder="Enter Password"
+                      name="password"
+                      value={password}
+                      onChange={(event) => setPassword(event.target.value)}
+                    />
+                  </div>
+                  <div className="input_text">
+                    <input
+                      type="password"
+                      placeholder="Re-enter Password"
+                      name="confirmPassord"
+                      value={confirmPassword}
+                      onChange={(event) =>
+                        setConfirmedPassword(event.target.value)
+                      }
+                    />
+                  </div>
+                  <div className="button">
+                    <Button color="primary" onClick={registrationHandler}>
+                      Register
+                    </Button>
+                  </div>
+                </form>
 
-      {!passwordsMatch && alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>
-          passwords don't match{" "}
-        </Alert>
-      )}
-      <Button color="primary" onClick={handlePasswordValidation}>
-        Register
-      </Button>
-    </div>
+                <hr />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 

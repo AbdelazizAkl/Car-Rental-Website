07d210ba69109b4bc5ba0e09568756f7500fee15
youssefwaheed123 <youssefwaheed@gmail.com>
2023-12-29 22:59:55 +0200

    cars page

--- car-rental-frontend/react-car-rental-website/src/pages/Cars.tsx
+++ car-rental-frontend/react-car-rental-website/src/pages/Cars.tsx
@@ -1,55 +1,74 @@
 import "../css/Car.css";
 import Navbar from "../components/NavBar";
-import carImage from "../images/cars/car-1.jpg";
 import CarCard from "../components/CarsCard";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import myCar from "../images/cars/car-1.jpg";
 
 const cars = () => {
+  interface Car {
+    id: string;
+    images: string; // Adjust type if images are an array or object
+    model: string;
+    year: string;
+    dailyPrice: string;
+    linkToDetails: string;
+  }
+
+  const [carsData, setCarsData] = useState<Car[]>([]);
+
+  useEffect(() => {
+    const fetchCars = async () => {
+      try {
+        const config = {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        };
+        const response = await axios.get("http://localhost:3000/cars/", config);
+
+        if (response.data) {
+          setCarsData(response.data.data); // Store fetched cars in state
+          console.log("Cars fetched successfully!");
+        } else {
+          console.error("Error fetching cars:", response.data.error);
+          // Handle the error appropriately, e.g., display an error message
+        }
+      } catch (error) {
+        console.error("Error fetching cars:", error);
+        // Handle the error appropriately, e.g., display an error message
+      }
+    };
+
+    fetchCars();
+  }, []);
+
+  console.log(carsData);
+
+  // Fetch cars on component mount
+
   return (
     <div>
       <div className="carsParent">
         <Navbar home={false} />
       </div>
-      {/* <section className="hero-wrap hero-wrap-2 js-fullheight"  data-stellar-background-ratio="0.5">
-      <img src={carImage} className="img-fluid" />
-      <div className="overlay"></div>
-      <div className="container">
-        <div className="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-          <div className="col-md-9 ftco-animate pb-5">
-          	<p className="breadcrumbs"><span className="mr-2"><a href="index.html">Home <i className="ion-ios-arrow-forward"></i></a></span> <span>Cars <i className="ion-ios-arrow-forward"></i></span></p>
-            <h1 className="mb-3 bread">Choose Your Car</h1>
-          </div>
-        </div>
-      </div>
-    </section> */}
-
       <section className="ftco-section bg-light">
         <div className="container">
           <div className="row">
-            <CarCard
-              myImage={carImage}
-              name="Mercedes Grand Sedan"
-              brand="Cheverolet"
-              price="500"
-              linkToDetails="/home"
-            />
-            <CarCard
-              myImage={carImage}
-              name="Mercedes Grand Sedan"
-              brand="Cheverolet"
-              price="500"
-              linkToDetails="/home"
-            />
-            <CarCard
-              myImage={carImage}
-              name="Mercedes Grand Sedan"
-              brand="Cheverolet"
-              price="500"
-              linkToDetails="/home"
-            />
+            {carsData.map((car) => (
+              <CarCard
+                key={`http://localhost:3000/images/${car.images}`} // Assuming each car has a unique ID
+                myImage={car.images}
+                name={car.model}
+                brand={car.year}
+                price={car.dailyPrice}
+                linkToDetails={""} // Assuming linkToDetails exists in car data
+              />
+            ))}
           </div>
         </div>
       </section>
     </div>
   );
 };
+
 export default cars;
